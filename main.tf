1c6bfd0fb938856f8c20b8558ac2d03561c6a4ed
Pushpak Vattam <vattampushpak243@gmail.com>
2024-06-25 07:25:56 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "main" {
         Effect    = "Allow"
         Sid       = ""
         Principal = {
-          Service = "eks.amazonaws.com"
+          Service = ["eks.amazonaws.com", "ec2.amazonaws.com"]
         }
       },
     ]
@@ -32,5 +32,48 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = var.subnets
   }
+}
 
+resource "aws_iam_role" "main-nodegroup" {
+  name = "${var.env}-eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = ["ec2.amazonaws.com"]
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.env}-eks-ng"
+  node_role_arn   = aws_iam_role.main.arn
+  subnet_ids      = var.subnets
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.node_count
+    max_size     = var.node_count
+    min_size     = var.node_count
+  }
 }
